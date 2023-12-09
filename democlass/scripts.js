384e13edc4f00910f6b78a893a1c1dec8766fc2f
samsom <samsomusa@icloud.com>
2023-12-09 12:47:08 -0600

    add

---
+++ democlass/scripts.js
@@ -0,0 +1,40 @@
+class student {
+    constructor(studentId, answers ){
+        this.studentId=studentId;
+        this.answers= answers;
+
+    }
+           addAnswer (question){
+        this.question.push(id,answer);
+
+    }
+}
+
+class Question{
+    constructor(qid, answer){
+        this.qid = qid;
+        history.answer = answer;
+    }
+    checkAnswer(answer){
+        if(student.addAnswer === "answer" ){
+            alert("answer")
+        }else{
+            return false;
+        }
+    }
+}
+
+class Quiz{
+    constructor(question){
+        this.question= new Map(questionArray)
+        this.student=[];
+    }
+    scoreStudentBysid(sid){
+        
+    }
+
+}
+
+
+
+
