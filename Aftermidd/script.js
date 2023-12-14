24f2785a78ca5602832baddc41080583cdffef37
samsom <samsomusa@icloud.com>
2023-12-13 18:29:56 -0600

    add

---
+++ Aftermidd/script.js
@@ -0,0 +1,9 @@
+// let div = document.createElement("div");
+// let p = document.createElement("p");
+// let text = document.createTextNode("it is p tage.");
+// // p.setAttriute 
+// div.appendChild(p);
+// p.appendChild(test);
+
+// document.body.appendChild(div);
+
