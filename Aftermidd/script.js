6c5cb7f3e3a30aac6fc3e4d929f910be0d0d0913
samsom <samsomusa@icloud.com>
2023-12-20 10:37:48 -0600

    add

--- Aftermidd/script.js
+++
@@ -1,9 +0,0 @@
-// let div = document.createElement("div");
-// let p = document.createElement("p");
-// let text = document.createTextNode("it is p tage.");
-// // p.setAttriute 
-// div.appendChild(p);
-// p.appendChild(test);
-
-// document.body.appendChild(div);
-
--- lab12/app.js
+++ lab12/app.js
@@ -1,2 +1,40 @@
-// let express= require('Express');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware for parsing JSON
+app.use(express.json());
+
+// Routes
+const usersRouter = require('./routes/users');
+const productsRouter = require('./routes/products');
+
+app.use('/users', usersRouter);
+app.use('/products', productsRouter);
+
+// 404 Page
+app.use((req, res) => {
+  //res.status(404).send('404 Page Not Found');
+  res.status(404).sendFile(__dirname + '/public/404-error.mp4'); //mp4 error video
+  
+});
+
+// Error Handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+  
+});
+
+
+
+
+
+
+
 
